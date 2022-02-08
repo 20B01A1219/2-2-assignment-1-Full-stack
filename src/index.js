e9ff263b2442390e1a5e53770ffa4a1d4381da41
Pallavi <sairamyasripallavi22@gmail.com>
2022-02-08 21:40:36 +0530

    2-2 first assignment

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import image from "./lizard.jfif";
+import "./styles.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Completedata() {
+    let share = "SHARE";
+    let learn = "LEARN MORE"
+  return (
+    <div className="firstdiv">
+      <div className="seconddiv">
+        <img src={image} />
+        <div className="thirdclass">
+          <h2>LIZARD</h2>
+          <p>
+            Lizard are a widespread group of squamate <br />
+            reptiles, with over 6000 species, ranging across <br />
+            all continents except Antartica
+          </p>
+          <a href="">{share}</a>
+          <a href = "" className="space">{learn}</a>
+        </div>
+      </div>
+    </div>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<Completedata />, document.getElementById("root"));
